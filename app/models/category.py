4fde61e27d9f9ec5ea44db6d373e132703ee62b9
Sebastien Dewaelle <sdewaelle@gmail.com>
2025-08-05 12:25:39 +0200

    Creation of the class Category

--- app/models/category.py
+++ app/models/category.py
@@ -3,9 +3,9 @@ from sqlmodel import Field, SQLModel
 from datetime import datetime, timezone
 
 
-class MenuBase(SQLModel, table=True):
+class CategoryBase(SQLModel, table=True):
     """
-    Menu model.
+    Category model.
 
     Args:
         SQLModel (SQLModel): Base class for SQLAlchemy models.
@@ -14,8 +14,4 @@ class MenuBase(SQLModel, table=True):
 
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str = Field(..., max_length=100, nullable=False, unique=True)
-    price: float = Field(default=0, ge=0, nullable=False)
-    category_id: Optional[int] = Field(default=None, foreign_key="category.id")
-    description: str = Field(..., max_length=255)
-    stock: int = Field(default=0, ge=0)
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
