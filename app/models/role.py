fc8f1ebbe05c8530547617f589235ef2693042cc
Alexis Halbot-Schoonaert <a.halbotschoonaert@gmail.com>
2025-08-05 13:36:20 +0000

    feat: :sparkles: db enum support for RoleType

--- app/models/role.py
+++ app/models/role.py
@@ -1,12 +1,20 @@
 from typing import Optional
 from sqlmodel import Field, SQLModel
 from enum import Enum
+import sqlalchemy as sa
 
-class RoleEnum(str, Enum):
+
+class RoleType(str, Enum):
     admin = "ADMIN"
     employee = "EMPLOYEE"
     customer = "CUSTOMER"
 
+
 class Role(SQLModel, table=True):
     id: Optional[int] = Field(..., primary_key=True)
-    role: str = Field(..., unique=True, nullable=False, ) # RoleEnum
+    role_type: str = Field(
+        ...,
+        unique=True,
+        nullable=False,
+        sa_column=sa.Column(sa.Enum(RoleType, name="role_type", create_type=True)),
+    )  # RoleEnum
