abd7cb66a545ee5c92d9b9d2a1b6b882f987c7b8
Sebastien Dewaelle <sdewaelle@gmail.com>
2025-08-05 16:42:52 +0200

    feat: :sparkles: alembic initial configuration

--- alembic/env.py
+++ alembic/env.py
@@ -1,29 +1,45 @@
+import os
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
+from sqlalchemy import create_engine
 from sqlalchemy import pool
+import sqlmodel
 
 from alembic import context
 
-# this is the Alembic Config object, which provides
+# Ajoutez ces imports pour charger le fichier .env
+from dotenv import load_dotenv
+
+# Chargez le fichier .env
+load_dotenv()
+
+# Ceci est le MetaData de SQLModel
+from sqlmodel import SQLModel
+
+# Ceci est l'endroit où vous importez vos modèles
+# par exemple, si vos modèles sont dans un fichier "models.py" dans le répertoire "app"
+from app.models.category import Category
+from app.models.menu import Menu
+
+# This is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
+# Interpret the config file for Python's standard logging.
+# This ensures that all loggers use the same configuration.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+# target_metadata est le MetaData que les migrations vont utiliser
+target_metadata = SQLModel.metadata
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+naming_convention = {
+    "ix": "ix_%(table_name)_%(column_0_label)s",
+    "uq": "uq_%(table_name)_%(column_0_name)s",
+    "ck": "ck_%(table_name)_%(constraint_name)s",
+    "fk": "fk_%(table_name)_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)",
+}
 
 
 def run_migrations_offline() -> None:
@@ -44,6 +60,7 @@ def run_migrations_offline() -> None:
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
+        naming_convention=naming_convention,
     )
 
     with context.begin_transaction():
@@ -51,21 +68,17 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
-
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    """Run migrations in 'online' mode."""
+    # Utilisez la variable d'environnement pour la chaîne de connexion
+    connectable = create_engine(os.getenv("DATABASE_URL"))
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection,
+            target_metadata=target_metadata,
+            compare_type=True,
+            naming_convention=naming_convention,
+            render_as_batch=True,
         )
 
         with context.begin_transaction():
