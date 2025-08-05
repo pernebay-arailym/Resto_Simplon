4ad329e4e4f4b1452437f88e1078bc7484883f1e
pernebay-arailym <pernebayarailym@gmail.com>
2025-08-05 15:06:35 +0200

    fixed the id and status

--- app/models/order.py
+++ app/models/order.py
@@ -1,15 +1,25 @@
-
 from sqlmodel import SQLModel, Field
 from datetime import datetime, timezone
 from typing import Literal
 from typing import Optional
-import uuid #A UUID is a 128-bit number used to uniquely identify information in computer systems.
+from enum import Enum
 
-class OrderBase(SQLModel, table=True): #Order model representing a customer's order in the restaurant system.
-    
+
+class OrderStatus(str, Enum):  # Enum for order status to ensure valid values.
+    CREATED = "Created"
+    PREPARING = "Preparing"
+    READY = "Ready"
+    SERVED = "Served"
+    CANCELLED = "Cancelled"
+    PAID = "Paid"
+
+
+class OrderBase(
+    SQLModel, table=True
+):  # Order model representing a customer's order in the restaurant system.
 
     id: Optional[int] = Field(default=None, primary_key=True)
-    client_id: uuid.UUID = Field(..., foreign_key="user.id")
+    client_id: int = Field(..., foreign_key="user.id")
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     total_price: float = Field(..., gt=0)
-    status: Literal["Created", "Preparing", "Ready", "Served", "Cancelled", "Paid"] = Field(...)
+    status: OrderStatus = Field(...)  # Using the OrderStatus enum for status field
