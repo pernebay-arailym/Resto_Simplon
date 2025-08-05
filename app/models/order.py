f3fc7eb7caba0d5fcd1119142dcca8b015ec3240
pernebay-arailym <pernebayarailym@gmail.com>
2025-08-05 13:42:58 +0200

    changed id to optional

--- app/models/order.py
+++ app/models/order.py
@@ -2,12 +2,13 @@
 from sqlmodel import SQLModel, Field
 from datetime import datetime, timezone
 from typing import Literal
+from typing import Optional
 import uuid #A UUID is a 128-bit number used to uniquely identify information in computer systems.
 
 class OrderBase(SQLModel, table=True): #Order model representing a customer's order in the restaurant system.
     
 
-    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    id: Optional[int] = Field(default=None, primary_key=True)
     client_id: uuid.UUID = Field(..., foreign_key="user.id")
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     total_price: float = Field(..., gt=0)
