6d48881a9f4aa9004114f0c7cffe2e7419a6be92
Alexis Halbot-Schoonaert <a.halbotschoonaert@gmail.com>
2025-08-06 14:13:43 +0000

    style: :art: Suppression commentaire devenu inutile

--- app/api/main.py
+++ app/api/main.py
@@ -4,7 +4,5 @@ from app.api.routes import user
 from app.api.routes import menu
 
 router = APIRouter()
-router.include_router(
-    user.router, prefix="/users"
-)  # param supplémentaire ?? , tags=["trainees"]
+router.include_router(user.router, prefix="/users")
 router.include_router(menu.router, prefix="/menus")
