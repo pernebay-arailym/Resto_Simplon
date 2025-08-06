0c13c1fc42d5cdf191c5bde4d37fa2e22a7d8d9d
pernebay-arailym <pernebayarailym@gmail.com>
2025-08-06 16:17:48 +0200

    feat: :sparkles: included router for order
    included router for order

--- app/api/main.py
+++ app/api/main.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from app.api.routes import user
 from app.api.routes import menu
 from app.api.routes import category
+from app.api.routes import order
 
 
 router = APIRouter()
@@ -11,3 +12,4 @@ router.include_router(
 )  # param supplémentaire ?? , tags=["trainees"]
 router.include_router(menu.router, prefix="/menus")
 router.include_router(category.router, prefix="/categories")
+router.include_router(order.router, prefix="/orders")
