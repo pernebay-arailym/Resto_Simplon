ad0b14937ad4a240be6452c2f678f2aefc11327e
Benalieur <benjamin.quinet59b@gmail.com>
2025-08-07 18:29:02 +0200

    fix: added user constraints

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -2,26 +2,30 @@ from pydantic import BaseModel, Field, EmailStr
 from typing import List, Optional
 from app.schemas.role_schema import RolePublic
 
+
 class UserBase(BaseModel):
-    username: str
-    email: EmailStr
-    first_name: str
-    last_name: str
-    adresse: str
-    phone: str
+    username: str = Field(..., max_length=50)
+    email: EmailStr = Field(..., max_length=255)
+    first_name: str = Field(..., max_length=50)
+    last_name: str = Field(..., max_length=50)
+    adresse: str = Field(...)
+    phone: str = Field(..., max_length=30)
+
 
 class UserCreate(UserBase):
-    password_hash: str
-    role_ids: List[int]
+    password_hash: str = Field(..., min_length=8)
+    role_ids: List[int] = Field(..., min_items=1)
+
 
 class UserUpdate(BaseModel):
-    username: Optional[str] = None
-    email: Optional[EmailStr] = None
-    first_name: Optional[str] = None
-    last_name: Optional[str] = None
+    username: Optional[str] = Field(None, max_length=50)
+    email: Optional[EmailStr] = Field(None, max_length=255)
+    first_name: Optional[str] = Field(None, max_length=50)
+    last_name: Optional[str] = Field(None, max_length=50)
     adresse: Optional[str] = None
-    phone: Optional[str] = None
-    role_ids: Optional[List[int]] = None  # Pour pouvoir update les rôles
+    phone: Optional[str] = Field(None, max_length=30)
+    role_ids: List[int] = Field(..., min_items=1)
+
 
 class UserPublic(UserBase):
     id: int
