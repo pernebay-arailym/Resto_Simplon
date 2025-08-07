e5f512ddf203c88341b5925680afe568967c61c6
Alexis Halbot-Schoonaert <a.halbotschoonaert@gmail.com>
2025-08-07 09:22:28 +0000

    feat: :sparkles: add role router to routers list

--- app/api/main.py
+++ app/api/main.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from app.api.routes import user
+from app.api.routes import role
 from app.api.routes import menu
 from app.api.routes import category
 from app.api.routes import order
@@ -8,6 +9,7 @@ from app.api.routes import order
 
 router = APIRouter()
 router.include_router(user.router, prefix="/users")
+router.include_router(role.router, prefix="/roles")
 router.include_router(menu.router, prefix="/menus")
 router.include_router(category.router, prefix="/categories")
 router.include_router(order.router, prefix="/orders")
