e2c71317d85dd664e0fb0109f3230ebf84cd84f7
pernebay-arailym <pernebayarailym@gmail.com>
2025-08-07 13:39:56 +0200

    feat: :sparkles: added order_detail to main in route

--- app/api/main.py
+++ app/api/main.py
@@ -4,10 +4,11 @@ from app.api.routes import user
 from app.api.routes import menu
 from app.api.routes import category
 from app.api.routes import order
-
+from app.api.routes import order_detail
 
 router = APIRouter()
 router.include_router(user.router, prefix="/users")
 router.include_router(menu.router, prefix="/menus")
 router.include_router(category.router, prefix="/categories")
 router.include_router(order.router, prefix="/orders")
+router.include_router(order_detail.router, prefix="/order-details")
