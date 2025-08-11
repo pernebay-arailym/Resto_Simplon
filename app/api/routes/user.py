1a110b584553a7cdd916f84e5131d4cec9bd8225
Sebastien Dewaelle <sdewaelle@gmail.com>
2025-08-11 10:10:50 +0200

    fix: :bug: Adding a mssing import

--- app/api/routes/user.py
+++ app/api/routes/user.py
@@ -5,7 +5,7 @@ from app.api.deps import SessionDep
 from app.auth.auth_handler import signJWT
 from app.models.user_role import User
 from app.models.order import OrderBase
-from app.schemas.user_schema import UserCreate, UserUpdate, UserPublic
+from app.schemas.user_schema import UserCreate, UserUpdate, UserPublic, UserLogin
 from app.schemas.order_schema import OrderPublic
 from app.crud import user_crud, role_crud
 
