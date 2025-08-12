0bfe6268c3239090ac5f7c32590a6506c89d640c
Alexis Halbot-Schoonaert <a.halbotschoonaert@gmail.com>
2025-08-12 09:45:17 +0000

    feat: :sparkles: In fastapi containers, all env vars are passed by docker. .env file is now used only by docker.

--- app/core/config.py
+++ app/core/config.py
@@ -1,15 +1,27 @@
 from pydantic_settings import BaseSettings
+import os
 
 
 class Settings(BaseSettings):
+    # POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+    # POSTGRES_DB: str = os.getenv("POSTGRES_DB")
+    # POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
+    # DATABASE_URL: str = os.getenv("DATABASE_URL")
+    # API_V1_STR: str = "/api/v1"
+    # JWT_SECRET: str = os.getenv("JWT_SECRET")
+    # JWT_ALGORITHM: str = os.getenv("JWT_ALGORITHM")
+
+    POSTGRES_USER: str
+    POSTGRES_DB: str
+    POSTGRES_PASSWORD: str
     DATABASE_URL: str
-    API_V1_STR: str = "/api/v1"
     JWT_SECRET: str
     JWT_ALGORITHM: str
+    API_V1_STR: str = "/api/v1"
 
-    class Config:
-        env_file = ".env"
-        extra = "ignore"  # option temporaire pour accepter un .env qui contient des variables non utilisées ici.
+    # class Config:
+    #     env_file = ".env"
+    #     extra = "ignore"  # option temporaire pour accepter un .env qui contient des variables non utilisées ici.
 
 
 settings = Settings()
